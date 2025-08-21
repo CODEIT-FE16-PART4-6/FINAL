ebef436600be4e46ed134c62f04493a036a91720
김이서 <12305025k@gmail.com>
2025-08-21 18:27:37 +0900

    fix: 오타, 타입 선언 오류 수정

--- src/types/schema/userSchema.ts
+++ src/types/schema/userSchema.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 //로그인 요청 스키마
 export const LoginRequestSchema = z.object({
-  email: z.string(),
-  password: z.string(),
+  email: z.email('올바른 이메일 형식을 입력해주세요.'),
+  password: z
+    .string('비밀번호는 필수 입력값입니다.')
+    .min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
 });
+
 //로그인 응답 스키마
 export const LoginResponseSchema = z.object({
   user: z.array(
@@ -11,7 +14,7 @@ export const LoginResponseSchema = z.object({
       id: z.number(),
       nickname: z.string(),
       email: z.string(),
-      profileImageUrl: z.string(),
+      profileImageUrl: z.string().nullable(),
       createdAt: z.string(),
       updatedAt: z.string(),
     }),
