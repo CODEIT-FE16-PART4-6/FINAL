a754bf0281784f34b856dff4f75cc98c2a38b676
leesangdal <asksa1256@gmail.com>
2025-08-21 17:17:04 +0900

    feat: Header - 스크롤 시 그림자 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,28 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import useScrollY from '@/utils/useScrollY';
+import { useEffect, useState } from 'react';
+import clsx from 'clsx';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const scrollY = useScrollY();
+
+  useEffect(() => {
+    scrollY > 100 ? setIsScrolled(true) : setIsScrolled(false);
+  }, [scrollY]);
+
   return (
-    <header className='sticky top-0 left-0 z-10 flex h-[70px] w-full items-center bg-white'>
+    <header
+      className={clsx(
+        'sticky top-0 left-0 z-10 flex h-[70px] w-full items-center bg-white transition-shadow',
+        {
+          shadow: isScrolled,
+        },
+      )}
+    >
       <div className='flex w-full items-center justify-between px-5 md:px-6 lg:mx-auto lg:w-[1200px]'>
         <Link href='/'>
           <Image src='/images/logo.svg' alt='Experia 로고' width={134} height={42} />
