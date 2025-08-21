88fd31990bd465b5476ef7fb0d13a7e8e9bf83e1
leesangdal <asksa1256@gmail.com>
2025-08-21 19:56:14 +0900

    feat: 로그인 후 알람, 사용자 정보 요소 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,10 +5,15 @@ import Link from 'next/link';
 import useScrollY from '@/utils/useScrollY';
 import { useEffect, useState } from 'react';
 import clsx from 'clsx';
+import AlarmIcon from '@/assets/icons/AlarmIcon.svg';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const scrollY = useScrollY();
+
+  const handleProfileClick = () => {
+    console.log('hello');
+  };
 
   useEffect(() => {
     scrollY > 100 ? setIsScrolled(true) : setIsScrolled(false);
@@ -17,7 +22,7 @@ const Header = () => {
   return (
     <header
       className={clsx(
-        'sticky top-0 left-0 z-10 flex h-[70px] w-full items-center bg-white transition-shadow border border-gray-300',
+        'sticky top-0 left-0 z-10 flex h-[70px] w-full items-center border border-gray-300 bg-white transition-shadow',
         {
           shadow: isScrolled,
         },
@@ -28,12 +33,38 @@ const Header = () => {
           <Image src='/images/logo.svg' alt='Experia 로고' width={134} height={42} />
         </Link>
         <nav className='flex gap-3 font-medium text-black'>
-          <Link href='/signin' className='nav-list'>
+          {/* 로그인 전 */}
+          {/* <Link href='/signin' className='nav-list'>
             로그인
           </Link>
           <Link href='/signup' className='nav-list'>
             회원가입
-          </Link>
+          </Link> */}
+
+          {/* 로그인 후 */}
+          <ul className='flex items-center divide-x divide-gray-300'>
+            <li className='pr-5'>
+              <button>
+                <AlarmIcon className='hover:text-primary text-gray-700 transition-colors' />
+              </button>
+            </li>
+            <li className='flex pl-5'>
+              <button
+                type='button'
+                className='hover:text-primary flex items-center gap-2.5 transition-colors'
+                onClick={handleProfileClick}
+              >
+                <Image
+                  src='/images/ProfileDefaultImg.png'
+                  alt='프로필 사진'
+                  width={32}
+                  height={32}
+                  className='rounded-full'
+                />
+                정만철
+              </button>
+            </li>
+          </ul>
         </nav>
       </div>
     </header>
