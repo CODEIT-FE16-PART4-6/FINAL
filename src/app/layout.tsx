6b22dbee38be39b4ee8117955bc4bf10993e5a70
leesangdal <asksa1256@gmail.com>
2025-08-22 13:31:52 +0900

    feat: layout에 Footer 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Providers } from '@/components/provider/Providers';
 import { pretendard } from '@/utils/fonts';
 import './globals.css';
 import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
