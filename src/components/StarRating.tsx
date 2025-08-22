6fcdcea1e28b1a25d4cca57c5376bc41da2f202a
Apologle <otl5994@naver.com>
2025-08-22 18:56:51 +0900

    feat:별점 기능에  이미지 추가 및 기본 기능 (hover , click )

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,20 +1,57 @@
 'use client';
+
+//hooks
 import { useState } from 'react';
 
-/*
-const StarRatingProps {
-    value?: number;
-    
-}
+// img
+import StarFilled from '@/assets/imgs/starFilled.svg';
+import StarEmpty from '@/assets/imgs/starEmpty.svg';
 
+interface StarRatingProps {
+  value?: number;
+  onChange?: (rating: number) => void;
+  size?: number;
+}
+/*
 const Star ()=>{
 
 
 }*/
-export const StarRating = () => {
-  const [currentRating, setCurrentRating] = useState(); // 현재 선택된 값.
-  const [hoverRating, setHoverRating] = useState(); //마우스 를 올렸을 때의 임시값
+
+/**
+ * 별점 컴포넌트
+ * @param value - 초기 별점 으로 넣을 값.(완전 처음엔 0으로 설정 됨)
+ * @param onChange -
+ * @param size -
+ * @returns - 리턴 값 설명
+ */
+export const StarRating = ({ value = 0, onChange, size = 32 }: StarRatingProps) => {
+  const [currentRating, setCurrentRating] = useState(value); // 현재 선택된 값.
+  const [hoverRating, setHoverRating] = useState(0); //마우스 를 올렸을 때의 임시값
   //[confirm, setConfirm] =useState(); //점수 확정 상태
 
-  return <>!!! starRatingTest</>;
+  const STARS = [1, 2, 3, 4, 5];
+
+  const handleClick = (star: number) => {
+    setCurrentRating(star);
+    onChange?.(star);
+  };
+
+  return (
+    <div>
+      <div className='flex h-14 w-14 gap-8'>
+        {STARS.map(star => (
+          <button
+            key={star}
+            type='button'
+            onClick={() => handleClick(star)}
+            onMouseEnter={() => setHoverRating(star)}
+            onMouseLeave={() => setHoverRating(0)}
+          >
+            {(hoverRating || currentRating) >= star ? <StarFilled /> : <StarEmpty />}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
