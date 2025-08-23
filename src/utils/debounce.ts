5bddebe05128e289e153f99cd177847c4b106893
leesangdal <asksa1256@gmail.com>
2025-08-23 23:49:13 +0900

    chore: debounce 함수 jsDoc 주석 정리

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -2,9 +2,9 @@ type Timer = ReturnType<typeof setTimeout> | null;
 
 /**
  *
- * @param fn {T} 지연 실행할 함수
- * @param timeout {number} [timeout=300] 지연 시간 (밀리초 단위)
- * @returns {T & { cancel: () => void }} 지연 실행, 지연 실행 취소 기능을 가진 함수 반환
+ * @param fn 지연 실행할 함수
+ * @param timeout [timeout=300] 지연 시간 (밀리초 단위)
+ * @returns 지연 실행, 지연 실행 취소 기능을 가진 함수 반환
  *
  */
 const debounce = <T extends (...args: Parameters<T>) => ReturnType<T>>(fn: T, timeout = 300) => {
