be898f882494f930a7cda48a0d1bf90953779c48
leesangdal <asksa1256@gmail.com>
2025-08-25 22:51:06 +0900

    feat: 체험 아이템 클릭 시 상세 페이지로 연결

--- src/components/activities/ActivityItem.tsx
+++ src/components/activities/ActivityItem.tsx
@@ -1,33 +1,39 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Activity } from '@/types/schema/activitiesSchema';
 
 const ActivityItem = ({ item }: { item: Activity }) => {
   return (
     <li className='max-w-none text-black lg:max-w-[282px]'>
-      <figure className='mb-4 aspect-square w-full overflow-hidden rounded-[20px]'>
-        <Image
-          src={item.bannerImageUrl}
-          alt={item.description}
-          width={282}
-          height={282}
-          className='h-full w-full object-cover'
-          blurDataURL={item.blurDataUrl!}
-          placeholder={item.blurDataUrl ? 'blur' : 'empty'}
-        />
-      </figure>
+      <Link href={`/activities/${item.id}`} className='group block'>
+        <figure className='relative mb-4 aspect-square w-full overflow-hidden rounded-[20px]'>
+          <Image
+            src={item.bannerImageUrl}
+            alt={item.description}
+            width={282}
+            height={282}
+            className='h-full w-full object-cover'
+            blurDataURL={item.blurDataUrl!}
+            placeholder={item.blurDataUrl ? 'blur' : 'empty'}
+          />
 
-      <span className='flex gap-1 text-base'>
-        <Image src='/icons/StarSmallIcon.svg' alt='별점' width={20} height={20} />
-        {item.rating}
-        <span className='text-gray-700'>(리뷰 {item.reviewCount}개)</span>
-      </span>
+          {/* hover 시 카드 전체 오버레이 */}
+          <div className='absolute inset-0 flex items-center justify-center bg-black/30 opacity-0 group-hover:opacity-100'></div>
+        </figure>
 
-      <h4 className='mt-2.5 mb-3.5 text-2xl font-semibold'>{item.title}</h4>
+        <span className='flex gap-1 text-base'>
+          <Image src='/icons/StarSmallIcon.svg' alt='별점' width={20} height={20} />
+          {item.rating}
+          <span className='text-gray-700'>(리뷰 {item.reviewCount}개)</span>
+        </span>
 
-      <h5 className='flex items-center text-2xl font-bold'>
-        ₩ {item.price}
-        <span className='ml-1 text-xl font-normal text-gray-900'>/ 인</span>
-      </h5>
+        <h4 className='mt-2.5 mb-3.5 text-2xl font-semibold'>{item.title}</h4>
+
+        <h5 className='flex items-center text-2xl font-bold'>
+          ₩ {item.price}
+          <span className='ml-1 text-xl font-normal text-gray-900'>/ 인</span>
+        </h5>
+      </Link>
     </li>
   );
 };
